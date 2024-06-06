45d5d4bdd572b1345faf53fb12c63a0cced4393b
Angel Shelton <angelshelton@Angels-MacBook-Air.local>
2024-06-06 12:37:35 -0700

    editing the css styling of the webpage

--- src/pages/login/signup/Signup.jsx
+++ src/pages/login/signup/Signup.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 
 export default function Signup() {
     return (
-        <div>Signup</div>
+        <div className=" signup">
+            <div className='card'>
+            <div className="left">
+                <h2>ZipSExtra <br />-</h2>
+                <p>
+                  Welcome to ZipS a social media app that allows you to stay connected with account followers allowing you to make posts and like them as well. 
+                </p>
+                <span>Don't Have An Account?</span>
+                <button className='btn btn-primary'>Register</button>
+            </div>
+            <form className="right">
+                  <input type="text" required placeholder='username' />
+                  <input type="password" required placeholder='password' />
+                  <button type='submit' className='btn'>Login</button>
+            </form>
+        </div>
+        </div>
     )
 }
