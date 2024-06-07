e346f2b4a2ae7a15fcf1ce053c026a116a399e23
Angel Shelton <angelshelton@Angels-MacBook-Air.local>
2024-06-06 20:01:37 -0700

    working on fixing the login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function Login() {
     return (
-        <div>Login</div>
+        <div className='login'>
+            <div className="left">
+                <h2>ZipSExtra</h2>
+                <p>
+                    Welcome user to ZipS social media webpage where you can connect, post, and like all of your fellow Zippers posts!
+                </p>
+                <span>Don't Have Any Account?</span>
+                <button className='btn btn-primary'>Register</button>
+            </div>
+            <form className="right">
+             <input type="text" required placeholder='username' />
+             <input type="password" required placeholder='password' />
+             <button type='submit'>Login</button>
+            </form>
+        </div>
     )
 }
