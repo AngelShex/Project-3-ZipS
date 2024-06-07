c694b62aae4115bbf0f5650e14c6349480275ecb
Angel Shelton <angelshelton@Angels-MacBook-Air.local>
2024-06-06 22:46:32 -0700

    Seperating two signin and update pages as

--- src/LayOut/LayOut.jsx
+++ src/LayOut/LayOut.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
 // Pages............
 import Login from '../pages/login/Login'
+import Signup from '../pages/signup/Signup'
+
 
 export default function LayOut() {
+    const router = createBrowserRouter([
+    {
+       path: '/login',
+       element : <Login />
+    },
+    {
+        path: '/signup',
+        element : <Signup />
+     },
+    ])
     return (
         <>
-        <Login />
+          <RouterProvider router={router} />
         </>
     )
 }
