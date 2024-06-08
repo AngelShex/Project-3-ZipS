45519a8a78bbc7c520c557df17ec94f915db6cd0
Watsonaj0316 <watsonalyece33@gmail.com>
2024-06-07 23:05:10 -0700

    Resolved merge conflict in Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
-import './home.css'; // Import the CSS file 
-
-=======
-import React from 'react'
->>>>>>> 046704ca748a6c46ecfa73d283a64cf9d5b4e111
+import './home.css'; // Import the CSS file
 
 export default function Home() {
     return (
