fbc518e660a9a743784bd0a51e527f2619d0d33d
Angel Shelton <angelshelton@Angels-MacBook-Air.local>
2024-06-10 10:41:52 -0700

    fixed Css bug within left bar folder

--- src/components/leftbar/LeftBar.jsx
+++ src/components/leftbar/LeftBar.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import './leftBar.css'
+
+//Components............
+import CurrentUser from '../../FackApis/CurrentUserData'
 
 export default function LeftBar() {
     return (
-        <div>LeftBar</div>
+        <div className='leftBar'>
+            <div className="left-container">
+                <div className="menu">
+                  <Link to='/profile/id'>
+                   <div className="user">
+                    <img src={CurrentUser.map(user=>(user.ProfieImage))} alt="" />
+                    <h4>Saskue Uchiha</h4>
+                   </div>
+                  </Link>
+                </div>
+                <div className="menu"></div>
+            </div>
+        </div>
     )
 }
