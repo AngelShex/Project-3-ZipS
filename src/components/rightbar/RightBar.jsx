5562da747b7172d4b83770355e00c056cc59cfbf
Angel Shelton <angelshelton@Angels-MacBook-Air.local>
2024-06-10 13:55:37 -0700

    connecting the friend requests and messages bar to the right side

--- src/components/rightbar/RightBar.jsx
+++ src/components/rightbar/RightBar.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from './rightBar.css';
+
+//Components ...............
+import Message from '../message/Message'
+import FirendReqe from '../firendReqe/firendReqe'
 
 export default function RightBar() {
     return (
-        <div>RightBar</div>
+        <div className='rightBar'>
+            <div className="rightbar-container">
+                <Message />
+                <FirendReqe />
+            </div>
+        </div>
     );
 }
